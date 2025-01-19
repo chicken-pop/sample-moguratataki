ba5f458725de9b23169d3031a6cbb42e1740dcd0
chicken <popcopy16youth0723@outlook.jp>
2025-01-20 05:58:41 +0900

    ユーティリティクラスの作成

---
+++ Assets/Scripts/Utility/DebugUtility.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Utility
+{
+    public class DebugUtility
+    {
+        public static void Log<T>(T message)
+        {
+            Debug.Log(message);
+        }
+
+        public static void LogWarning<T>(T message)
+        {
+            Debug.LogWarning(message);
+        }
+
+        public static void LogError<T>(T message)
+        {
+            Debug.LogError(message);
+        }       
+    }
+}
+
+
---
+++ Assets/Scripts/Utility/SingletonMonoBehaviour.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+namespace Utility
+{
+    public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = (T)FindObjectOfType(typeof(T));
+                    if (Instance == null)
+                    {
+                        SetupInstance();
+                    }
+                    else
+                    {
+                        string typeName = typeof(T).Name;
+                        Debug.Log("[Singleton] " + typeName + " instance already created: " +
+                                _instance.gameObject.name);
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        // シーン内だけのSingletonとする
+        protected static bool isSceneSingleton = false;
+
+        public virtual void Awake()
+        {
+            RemoveDuplicates();
+        }
+
+        /// <summary>
+        /// Instanceのセットアップ
+        /// </summary>
+        private static void SetupInstance()
+        {
+            _instance = (T)FindObjectOfType(typeof(T));
+
+            if (_instance == null)
+            {
+                GameObject gameObj = new GameObject();
+                gameObj.name = typeof(T).Name;
+
+                _instance = gameObj.AddComponent<T>();
+                if (!isSceneSingleton)
+                {
+                    DontDestroyOnLoad(gameObj);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Instanceが複数シーンにないか確認
+        /// </summary>
+        private void RemoveDuplicates()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                // シーン内だけのSingletonとする
+                if (!isSceneSingleton)
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
+
